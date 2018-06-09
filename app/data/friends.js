0b219d5818d0dac26d23217ac04bf05b69692e26
abhilashalk <abhilulk@gmail.com>
2018-06-09 13:07:08 -0400

    assets folder

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+//Friends info with respective survey answers.
+var friendsArray = [{
+        name: "Anju",
+        photo: "",
+        scores: [
+            5,
+            4,
+            3,
+            4,
+            5,
+            1,
+            2,
+            5,
+            3,
+            4
+        ]
+
+    }
+
+
+
+
+
+
+
+
+
+
+];
